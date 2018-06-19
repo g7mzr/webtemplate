1422463bb8308de2695df7cb2c07d823d7871594
Sandy McNeil <g7mzrdev@gmail.com>
2018-06-19 19:06:18 +0100

    Update page.php to use application class
    Modify page.php by removing all calls to
    individual classes and replace with application
    class.
    
    Signed-off-by: Sandy McNeil <g7mzrdev@gmail.com>

--- base/page.php
+++ base/page.php
@@ -14,33 +14,17 @@
 // Include the Globals
 require_once "../includes/global.php";
 
-// Create the Smart Template object
-$tpl = new \webtemplate\application\SmartyTemplate;
-
-//Set up the correct language and associated templates
-$languageconfig = \webtemplate\general\General::getconfigfile($tpl->getConfigDir());
-$tpl->assign('CONFIGFILE', $languageconfig);
-$tpl->configLoad($languageconfig);
-$language = $tpl->getConfigVars('Language');
-$templateArray = $tpl->getTemplateDir();
-$tpl->setTemplateDir($templateArray[0] . '/' . $language);
-$tpl->setCompileId($language);
-putenv("LANGUAGE=$language");
-setlocale(LC_ALL, $language);
-bindtextdomain('messages', '../locale');
-textdomain('messages');
-
-// Get the Database login information
-\webtemplate\application\WebTemplateCommon::loadDSN($tpl, $dsn);
-
-// Loginto the Database for all classes
-$db =& \webtemplate\db\DB::load($dsn);
-if (\webtemplate\general\General::isError($db)) {
-    // Unable to Connect to the Database
+// Create a WEBTEMPLATE CLASS
+try {
+    $app = new \webtemplate\application\Application();
+} catch (\Throwable $e) {
+    // Create the Smart Template object
+    $tpl = new \webtemplate\application\SmartyTemplate;
     $template = 'global/error.tpl';
-    $msg = gettext("Unable to Connect to the Database.\n\n");
+    $msg = $e->getMessage();
+    $msg .= "\n\n";
     $msg .= gettext("Please Contact your Adminstrator");
-    $header =  gettext("Unable to Connect to the Database");
+    $header =  gettext("Application Error");
     $tpl->assign('ERRORMSG', $msg);
     $tpl->assign('HEADERMSG', $header);
     $dateArray = getdate();
@@ -49,86 +33,62 @@ if (\webtemplate\general\General::isError($db)) {
     exit();
 }
 
-//$tpl->debugging = true;
-
-//Create new config class
-$configdir = $tpl->getConfigDir(0);
-$config = new \webtemplate\config\Configure($configdir);
-
-//Create the logclass
-$log = new \webtemplate\general\Log(
-    $config->read('param.admin.logging'),
-    $config->read('param.admin.logrotate')
-);
-
+// Set up the Language for translations
+$language = $app->language();
+\putenv("LANGUAGE=$language");
+\setlocale(LC_ALL, $language);
+\bindtextdomain('messages', '../locale');
+\textdomain('messages');
 
 // Load the menu and assign it to a SMARTY Variable
-$mainmenu = $config->readMenu('mainmenu');
-$tpl->assign('MAINMENU', $mainmenu);
-
-// Initalise the session variables
-$session = new \webtemplate\application\Session(
-    $config->read('param.cookiepath'),
-    $config->read('param.cookiedomain'),
-    $config->read('param.users.autologout'),
-    $tpl,
-    $db
-);
-
+$mainmenu = $app->config()->readMenu('mainmenu');
+$app->tpl()->assign('MAINMENU', $mainmenu);
 
 /* Send the HTTP Headers required by the application */
 $headerResult = \webtemplate\application\Header::sendHeaders();
 if ($headerResult == false) {
     // Log error is headers not sent
-    $log->error(basename(__FILE__) . ":  Failed to send HTTP Headers");
+    $app->log()->error(basename(__FILE__) . ":  Failed to send HTTP Headers");
 }
 
 // Check if the user is logged in.
-if ($session->getUserName() == '') {
+if ($app->session()->getUserName() == '') {
     // Tell the templates the user has not logged in.
-    $tpl->assign('LOGIN', true);
+    $app->tpl()->assign('LOGIN', true);
 
     // Get the Default style
     $stylesheetarray = array();
-    $stylesheetarray[] = 'style/' . $config->read('pref.theme.value') . '/main.css';
-    $stylesheetarray[] = 'style/' . $config->read('pref.theme.value') . '/page.css';
-    $tpl->assign('STYLESHEET', $stylesheetarray);
+    $stylesheetarray[] = 'style/' . $app->config()->read('pref.theme.value') . '/main.css';
+    $stylesheetarray[] = 'style/' . $app->config()->read('pref.theme.value') . '/page.css';
+    $app->tpl()->assign('STYLESHEET', $stylesheetarray);
 } else {
     // Tell the templates the user has logged in.  This will display the menus
-    $tpl->assign('LOGIN', false);
-
-
-    // Configure the correct user and their permissions
-    $user = new \webtemplate\users\User($db);
-    $user->register($session->getUserName(), $config->read('pref'));
-
-    // Get the users groups
-    $userGroups = new \webtemplate\users\Groups($db, $user->getUserId());
+    $app->tpl()->assign('LOGIN', false);
 
     // Get the users select style
     $stylesheetarray = array();
-    $stylesheetarray[] = 'style/' . $user->getUserTheme() . '/main.css';
-    $stylesheetarray[] = 'style/' . $user->getUserTheme() . '/page.css';
-    $tpl->assign('STYLESHEET', $stylesheetarray);
+    $stylesheetarray[] = 'style/' . $app->user()->getUserTheme() . '/main.css';
+    $stylesheetarray[] = 'style/' . $app->user()->getUserTheme() . '/page.css';
+    $app->tpl()->assign('STYLESHEET', $stylesheetarray);
 
 
     // Users real name for displaying on web page
-    $tpl->assign('USERNAME', $user->getRealName());
+    $app->tpl()->assign('USERNAME', $app->user()->getRealName());
 
     // Assign AdminAccess Rights to the template
-    $tpl->assign('ADMINACCESS', $userGroups->getAdminAccess());
+    $app->tpl()->assign('ADMINACCESS', $app->usergroups()->getAdminAccess());
 }
 
 // Set the Sys admin email address
-$tpl->assign("SYSADMINEMAIL", $config->read("param.maintainer"));
+$app->tpl()->assign("SYSADMINEMAIL", $app->config()->read("param.maintainer"));
 
 // Check if the docbase parameter is set and the document files are available
 $docsAvailable = \webtemplate\general\General::checkdocs(
-    $config->read('param.docbase'),
+    $app->config()->read('param.docbase'),
     $language
 );
 
-$tpl->assign("DOCSAVAILABLE", $docsAvailable);
+$app->tpl()->assign("DOCSAVAILABLE", $docsAvailable);
 
 
 //DO WHAT EVER THIS MODULE NEEDS TO DO  ^[\w,\s-]+\.[A-Za-z]{3}$
@@ -143,7 +103,7 @@ if (\filter_input(INPUT_GET, "id") !== null) {
         $template = 'pages/' . $fileName . '.tpl';
 
         // Check template exists
-        if ($tpl->templateExists($template)) {
+        if ($app->tpl()->templateExists($template)) {
             $pageFound = true;
         }
     }
@@ -151,18 +111,18 @@ if (\filter_input(INPUT_GET, "id") !== null) {
 
 // Display an error Message if a page is not found.
 if ($pageFound == false) {
-    $logmsg = $user-> getUserName() . " requested " .   $fileName;
-    $log->security("page.php: " . $logmsg);
+    $logmsg = $app->user()->getUserName() . " requested " .   $fileName;
+    $app->log()->security("page.php: " . $logmsg);
     $template = 'global/error.tpl';
     $msg = gettext("The Page you have requested ($fileName) was not found.");
     $header =  gettext("Page Not Found");
-    $tpl->assign('ERRORMSG', $msg);
-    $tpl->assign('HEADERMSG', $header);
+    $app->tpl()->assign('ERRORMSG', $msg);
+    $app->tpl()->assign('HEADERMSG', $header);
 }
 
 // Get the year for the Copyright Statement
 $dateArray = getdate();
-$tpl->assign('YEAR', "$dateArray[year]");
+$app->tpl()->assign('YEAR', "$dateArray[year]");
 
 // Display the Web Page
-$tpl->display($template);
+$app->tpl()->display($template);
