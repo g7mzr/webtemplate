b72d96b93dce135346b21ae460c57a54ff97ca56
Sandy McNeil <g7mzrdev@gmail.com>
2018-06-19 19:02:21 +0100

    Update editconfig.php to use application class
    Modify editconfig.php by removing all calls to
    individual classes and replace with application
    class.
    
    Signed-off-by: Sandy McNeil <g7mzrdev@gmail.com>

--- base/editconfig.php
+++ base/editconfig.php
@@ -25,34 +25,17 @@ require_once "../includes/global.php";
  * @link      http://www.g7mzr.demon.co.uk
  */
 
-// Create the Smart Template object
-$tpl = new \webtemplate\application\SmartyTemplate;
-
-
-//Set up the correct language and associated templates
-$languageconfig = \webtemplate\general\General::getconfigfile($tpl->getConfigDir());
-$tpl->assign('CONFIGFILE', $languageconfig);
-$tpl->configLoad($languageconfig);
-$language = $tpl->getConfigVars('Language');
-$templateArray = $tpl->getTemplateDir();
-$tpl->setTemplateDir($templateArray[0] . '/' . $language);
-$tpl->setCompileId($language);
-putenv("LANGUAGE=$language");
-setlocale(LC_ALL, $language);
-bindtextdomain('messages', '../locale');
-textdomain('messages');
-
-// Get the Database login information
-\webtemplate\application\WebTemplateCommon::loadDSN($tpl, $dsn);
-
-// Loginto the Database for all classes
-$db = \webtemplate\db\DB::load($dsn);
-if (\webtemplate\general\General::isError($db)) {
-    // Unable to Connect to the Database
+// Create a WEBTEMPLATE CLASS
+try {
+    $app = new \webtemplate\application\Application();
+} catch (\Throwable $e) {
+    // Create the Smart Template object
+    $tpl = new \webtemplate\application\SmartyTemplate;
     $template = 'global/error.tpl';
-    $msg = gettext("Unable to Connect to the Database.\n\n");
+    $msg = $e->getMessage();
+    $msg .= "\n\n";
     $msg .= gettext("Please Contact your Adminstrator");
-    $header =  gettext("Unable to Connect to the Database");
+    $header =  gettext("Application Error");
     $tpl->assign('ERRORMSG', $msg);
     $tpl->assign('HEADERMSG', $header);
     $dateArray = getdate();
@@ -61,127 +44,91 @@ if (\webtemplate\general\General::isError($db)) {
     exit();
 }
 
-//Create new config class
-$configdir = $tpl->getConfigDir(0);
-$config = new \webtemplate\config\Configure($configdir);
-
-//Create New  Parameters Editing class
-$editParams = new \webtemplate\admin\Parameters($config);
-
-
-//Create the logclass
-$log = new \webtemplate\general\Log(
-    $config->read('param.admin.logging'),
-    $config->read('param.admin.logrotate')
-);
-
-
-// Create new Token Class
-$token = new \webtemplate\general\Tokens($tpl, $db);
-
-//$tpl->debugging = true;
+// Set up the Language for translations
+$language = $app->language();
+\putenv("LANGUAGE=$language");
+\setlocale(LC_ALL, $language);
+\bindtextdomain('messages', '../locale');
+\textdomain('messages');
 
 // Load the menu and assign it to a SMARTY Variable
-$mainmenu = $config->readMenu('mainmenu');
-$tpl->assign('MAINMENU', $mainmenu);
-
-// Initalise the session variables
-$session = new \webtemplate\application\Session(
-    $config->read('param.cookiepath'),
-    $config->read('param.cookiedomain'),
-    $config->read('param.users.autologout'),
-    $tpl,
-    $db
-);
-
+$mainmenu = $app->config()->readMenu('mainmenu');
+$app->tpl()->assign('MAINMENU', $mainmenu);
 
 /* Send the HTTP Headers required by the application */
 $headerResult = \webtemplate\application\Header::sendHeaders();
 if ($headerResult == false) {
     // Log error is headers not sent
-    $log->error(basename(__FILE__) . ":  Failed to send HTTP Headers");
+    $app->log()->error(basename(__FILE__) . ":  Failed to send HTTP Headers");
 }
 
 // Check if the user is logged in.
-if ($session->getUserName() == '') {
+if ($app->session()->getUserName() == '') {
     //The user is not logged in. Display a login screen
     $headerResult = \webtemplate\application\Header::sendRedirect('index.php');
     if ($headerResult == false) {
         // Log error is headers not sent
-        $log->error(basename(__FILE__) . ":  Failed to send Redirect HTTP Header");
+        $app->log()->error(
+            basename(__FILE__) . ":  Failed to send Redirect HTTP Header"
+        );
     }
     exit();
 }
 
-// Configure the correct user and their permissions
-$user = new \webtemplate\users\User($db);
-$result = \webtemplate\general\General::isError(
-    $user->register($session->getUserName(), $config->read('pref'))
-);
-if ($result) {
-    $log->error(
-        basename(__FILE__) .
-        ": Failed To Register logged in user $username"
-    );
-}
-
-// Get the users groups
-$userGroups = new \webtemplate\users\Groups($db, $user->getUserId());
-
 // Get the users select style
 $stylesheetarray = array();
-$stylesheetarray[] = '/style/' . $user->getUserTheme() . '/main.css';
-$stylesheetarray[] = '/style/' . $user->getUserTheme() . '/editconfig.css';
-$tpl->assign('STYLESHEET', $stylesheetarray);
+$stylesheetarray[] = '/style/' . $app->user()->getUserTheme() . '/main.css';
+$stylesheetarray[] = '/style/' . $app->user()->getUserTheme() . '/editconfig.css';
+$app->tpl()->assign('STYLESHEET', $stylesheetarray);
 
 // Set the Sys admin email address
-$tpl->assign("SYSADMINEMAIL", $config->read("param.maintainer"));
+$app->tpl()->assign("SYSADMINEMAIL", $app->config()->read("param.maintainer"));
 
 // Tell the templates the user has logged in.  This will display the menus
-$tpl->assign('LOGIN', false);
+$app->tpl()->assign('LOGIN', false);
 
 // Users real name for displaying on web page
-$tpl->assign("USERNAME", $user->getRealName());
+$app->tpl()->assign("USERNAME", $app->user()->getRealName());
 
 // Assign AdminAccess Rights to the template
-$tpl->assign('ADMINACCESS', $userGroups->getAdminAccess());
+$app->tpl()->assign('ADMINACCESS', $app->usergroups()->getAdminAccess());
 
 // Assign zoom textboxes
-$tpl->assign("ZOOMON", $user->getUserZoom());
+$app->tpl()->assign("ZOOMON", $app->user()->getUserZoom());
 
 // Check if the docbase parameter is set and the document files are available
 $docsAvailable = \webtemplate\general\General::checkdocs(
-    $config->read('param.docbase'),
+    $app->config()->read('param.docbase'),
     $language
 );
-$tpl->assign("DOCSAVAILABLE", $docsAvailable);
+$app->tpl()->assign("DOCSAVAILABLE", $docsAvailable);
 
 // Get the year for the Copyright Statement
 $dateArray = getdate();
-$tpl->assign("YEAR", "$dateArray[year]");
+$app->tpl()->assign("YEAR", "$dateArray[year]");
 
 
 // Check the user has permission to run this module
-if (!$userGroups->checkGroup("admin") == true) {
+if (!$app->usergroups()->checkGroup("admin") == true) {
     // User is not allowed to display the admin page
-    $log->security(
-        $session->getUserName() .
+    $app->log()->security(
+        $app->session()->getUserName() .
         gettext(" attempted to access ") .
         basename(__FILE__)
     );
     $template = 'global/error.tpl';
     $msg = gettext("You are not authorised to access this page.");
     $header =  gettext("Authorisation Required");
-    $tpl->assign('ERRORMSG', $msg);
-    $tpl->assign('HEADERMSG', $header);
+    $app->tpl()->assign('ERRORMSG', $msg);
+    $app->tpl()->assign('HEADERMSG', $header);
     $dateArray = getdate();
-    $tpl->assign("YEAR", "$dateArray[year]");
-    $tpl->display($template);
+    $app->tpl()->assign("YEAR", "$dateArray[year]");
+    $app->tpl()->display($template);
     exit();
 }
 
 //Set up the page title.
-$tpl->assign("PAGETITLE", gettext("Edit Configuration"));
+$app->tpl()->assign("PAGETITLE", gettext("Edit Configuration"));
 
 // Set page to open on the requested section.
 // If no section selected default to required .
@@ -203,10 +150,10 @@ if (preg_match($sectionRegex, $tempSection, $regs) == true) {
 }
 
 // Tell template which section to load
-$tpl->assign("SECTION", $section);
+$app->tpl()->assign("SECTION", $section);
 
 // Get the pagelist
-$pagelist = $editParams->getPageList();
+$pagelist = $app->parameters()->getPageList();
 
 // Select the template
 $template = $pagelist[$section]['template'];
@@ -214,10 +161,10 @@ $template = $pagelist[$section]['template'];
 // Set the selected section
 $pagelist[$section]['selected'] = true;
 
-$editParams->setSection($section);
+$app->parameters()->setSection($section);
 
 //Set the old Parameters array.  This is a tempory FIX
-$parameters = $config->read('param');
+$parameters = $app->config()->read('param');
 
 // Check if user has requested that the parameters be updated.
 // If $_POST['action'] is not set display the current values
@@ -237,103 +184,109 @@ if (\filter_input(INPUT_POST, 'action') !== null) {
 if ($action == "save") {
     // Abort the script if the session and page tokens are not the same.
     $inputToken = \filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
-    $uid = $user->getUserId();
-    if ($token->verifyToken($inputToken, 'CONFIG', $uid) == false) {
+    $uid = $app->user()->getUserId();
+    if ($app->tokens()->verifyToken($inputToken, 'CONFIG', $uid) == false) {
         $template = "global/error.tpl";
         $msg = gettext("Security Failure. Parameters not updated");
         $header = gettext("Token Check Failure");
-        $tpl->assign("ERRORMSG", $msg);
-        $tpl->assign("HEADERMSG", $header);
-        $tpl->display($template);
+        $app->tpl()->assign("ERRORMSG", $msg);
+        $app->tpl()->assign("HEADERMSG", $header);
+        $app->tpl()->display($template);
         exit();
     }
 
-    $dataok = $editParams->validateParameters($_POST);
+    $dataok = $app->parameters()->validateParameters($_POST);
 
     // Check if the datavalidation passed
     if ($dataok == true) {
         // Check if any of the parameters have changed
 
-        $datachanged = $editParams->checkParametersChanged(
-            $config->read('param')
+        $datachanged = $app->parameters()->checkParametersChanged(
+            $app->config()->read('param')
         );
         if ($datachanged == true) {
             // At least one required parameter has changed.
             // Set the Parameters array with the new values.
-            $parameters = $editParams->getCurrentParameters();
+            $parameters = $app->parameters()->getCurrentParameters();
 
             // Save the new parameters
-            $configDir = $tpl->getConfigDir();
-            if ($editParams->saveParamFile($configDir[0]) == true) {
+            $configDir = $app->tpl()->getConfigDir();
+            if ($app->parameters()->saveParamFile($configDir[0]) == true) {
                 // Parameters saved.  Populate the Change Message
-                $msg = $editParams->getLastMsg();
+                $msg = $app->parameters()->getLastMsg();
             } else {
                 // An error was encountered saving the parameters file
                 $msg = gettext("Configuration Parameters not Saved") . "\n";
             }
-            $tpl->assign("MSG", $msg);
+            $app->tpl()->assign("MSG", $msg);
         } else {
             // No changes were made to the required parameters
             $msg = gettext("You Have Not Made Any Changes") . "\n";
-            $tpl->assign("MSG", $msg);
+            $app->tpl()->assign("MSG", $msg);
         }
     } else {
         // At least one of the entered parameters was invalid.
         // Populate the error message
-        $msg = $editParams->getLastMsg();
+        $msg = $app->parameters()->getLastMsg();
 
         // Assign the message to the template.
-        $tpl->assign("MSG", $msg);
+        $app->tpl()->assign("MSG", $msg);
 
         // Load the Parameters array with the invalid data.
-        $parameters = $editParams->getCurrentParameters();
+        $parameters = $app->parameters()->getCurrentParameters();
     }
 }
 // If an invalid command has been chosen display this in the MSG box
 if ($action == "invalid") {
-    $tpl->assign("MSG", gettext("You have made an invalid command"));
+    $app->tpl()->assign("MSG", gettext("You have made an invalid command"));
 }
 
 // Assign the required parameters to the template
-$tpl->assign("URLBASE", $parameters['urlbase']);
-$tpl->assign("MAINTAINER", $parameters['maintainer']);
-$tpl->assign("DOCBASEURL", $parameters['docbase']);
-$tpl->assign("COOKIEDOMAIN", $parameters['cookiedomain']);
-$tpl->assign("COOKIEPATH", $parameters['cookiepath']);
+$app->tpl()->assign("URLBASE", $parameters['urlbase']);
+$app->tpl()->assign("MAINTAINER", $parameters['maintainer']);
+$app->tpl()->assign("DOCBASEURL", $parameters['docbase']);
+$app->tpl()->assign("COOKIEDOMAIN", $parameters['cookiedomain']);
+$app->tpl()->assign("COOKIEPATH", $parameters['cookiepath']);
 
 // Assign the Admin Parameters to the template
-$tpl->assign("LOGGING", $parameters['admin']['logging']);
-$tpl->assign("LOGROTATE", $parameters['admin']['logrotate']);
-$tpl->assign("NEW_WINDOW_ON", $parameters['admin']['newwindow']);
-$tpl->assign("MAXRECORDS", $parameters['admin']['maxrecords']);
+$app->tpl()->assign("LOGGING", $parameters['admin']['logging']);
+$app->tpl()->assign("LOGROTATE", $parameters['admin']['logrotate']);
+$app->tpl()->assign("NEW_WINDOW_ON", $parameters['admin']['newwindow']);
+$app->tpl()->assign("MAXRECORDS", $parameters['admin']['maxrecords']);
 
 // Assign the auth parameters to the templat
-$tpl->assign("CREATE_ACCOUNT_ON", $parameters['users']['newaccount']);
-$tpl->assign("NEW_PASSWORD_ON", $parameters['users']['newpassword']);
-$tpl->assign("REGEXP", $parameters['users']['regexp']);
-$tpl->assign("REGEXPDESC", $parameters['users']['regexpdesc']);
-$tpl->assign("PASSWDSTRENGTH", $parameters['users']['passwdstrength']);
-$tpl->assign("PASSWDAGE", $parameters['users']['passwdage']);
-$tpl->assign("AUTOCOMPLETE", $parameters['users']['autocomplete']);
-$tpl->assign("AUTOLOGOUT", $parameters['users']['autologout']);
+$app->tpl()->assign("CREATE_ACCOUNT_ON", $parameters['users']['newaccount']);
+$app->tpl()->assign("NEW_PASSWORD_ON", $parameters['users']['newpassword']);
+$app->tpl()->assign("REGEXP", $parameters['users']['regexp']);
+$app->tpl()->assign("REGEXPDESC", $parameters['users']['regexpdesc']);
+$app->tpl()->assign("PASSWDSTRENGTH", $parameters['users']['passwdstrength']);
+$app->tpl()->assign("PASSWDAGE", $parameters['users']['passwdage']);
+$app->tpl()->assign("AUTOCOMPLETE", $parameters['users']['autocomplete']);
+$app->tpl()->assign("AUTOLOGOUT", $parameters['users']['autologout']);
 
 // Assign the email parameters to the template
-$tpl->assign(
+$app->tpl()->assign(
     "MAIL_DELIVERY_METHOD",
     $parameters['email']['smtpdeliverymethod']
 );
-$tpl->assign("EMAILADDRESS", $parameters['email']['emailaddress']);
-$tpl->assign("SMTPSERVER", $parameters['email']['smtpserver']);
-$tpl->assign("SMTPUSERNAME", $parameters['email']['smtpusername']);
-$tpl->assign("SMTPPASSWD", $parameters['email']['smtppassword']);
-$tpl->assign("SMTP_DEBUG_ON", $parameters['email']['smtpdebug']);
+$app->tpl()->assign("EMAILADDRESS", $parameters['email']['emailaddress']);
+$app->tpl()->assign("SMTPSERVER", $parameters['email']['smtpserver']);
+$app->tpl()->assign("SMTPUSERNAME", $parameters['email']['smtpusername']);
+$app->tpl()->assign("SMTPPASSWD", $parameters['email']['smtppassword']);
+$app->tpl()->assign("SMTP_DEBUG_ON", $parameters['email']['smtpdebug']);
 
 // Asign the pagelist to the template
-$tpl->assign("PAGELIST", $pagelist);
+$app->tpl()->assign("PAGELIST", $pagelist);
 
 // Create the token for checking the page authenticition
-$localtoken = $token->createToken($user->getUserId(), 'CONFIG', 1, ''. true);
-$tpl->assign("TOKEN", $localtoken);
+$localtoken = $app->tokens()->createToken(
+    $app->user()->getUserId(),
+    'CONFIG',
+    1,
+    '',
+    true
+);
+$app->tpl()->assign("TOKEN", $localtoken);
 
 // Display the Web Page
-$tpl->display($template);
+$app->tpl()->display($template);
